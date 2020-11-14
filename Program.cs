73fda881d6f8a84d06337d89f495e782e6eeec71
David Ebbo <david.ebbo@gmail.com>
2020-11-14 10:43:32 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ExifLibrary;
+using System;
+
+namespace FixPhotoDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var file = ImageFile.FromFile("path_to_image");
+        }
+    }
+}
